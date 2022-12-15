05d399099bc9101015a37e936d1f52327766fd54
nitzansaar <112202258+nitzansaar@users.noreply.github.com>
2022-12-14 16:39:48 -0800

    Assignment 7

--- ReadMe.java
+++ ReadMe.java
@@ -1,5 +1,5 @@
 /**
- * This program works by firstly calling the main method. Afterwards an options menu will appear and you can choose from
+ * How to run this code. To use this program, first call the main method. Afterwards an options menu will appear and you can choose from
  * the 12 options. The options are: (1)Add song to database, (2)Add artist to database, (3)Add album to database,
  * (4)Create your own playlist, (5)Generate playlist based on genre and print to XML file, (6)Generate playlist based on
  * artist and print to XML file, (7)Remove song from database, (8)Display songs table in database, (9)Display artists table
@@ -7,3 +7,8 @@
  *  takes advantage of java's object oriented interface. I chose to do my project like this because I want my user to have
  *  as much flexibility as possible. I modeled this project off of my own preferences for how a music library should function.
  */
+public class ReadMe {
+
+}
+
+
